8996b0682c35c6f7cf7bd604ead871d4f9e9be35
Kevin Wollowski <k-wollowsi@live.de>
2022-04-01 12:14:40 -0700

    Update proxy.sol

--- packages/contracts/contracts/TestContracts/DappSys/proxy.sol
+++ packages/contracts/contracts/TestContracts/DappSys/proxy.sol
@@ -22,7 +22,7 @@
 
 pragma solidity ^0.8.11;
 
-abstract contract DSAuthority {
+contract DSAuthority {
     function canCall(
         address src, address dst, bytes4 sig
     ) public view virtual returns (bool);
