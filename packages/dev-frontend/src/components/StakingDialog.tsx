c02aea1aaff98a11ead80d49718ea0a9d99bebc2
asimpk <asimpk.dev@gmail.com>
2022-04-13 16:22:43 +0500

    multi-collateral func added, comp design updated

---
+++ packages/dev-frontend/src/components/StakingDialog.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Heading, Flex, Card, Button, Box } from "theme-ui";
+import { Stability } from "../components/Stability/Stability";
+import { Trove } from "./Trove/Trove";
+
+import { Icon } from "./Icon";
+
+type DialogIntent = "success" | "warning" | "danger" | "info";
+
+type DialogProps = {
+  intent?: DialogIntent;
+  title: string;
+  icon?: React.ReactNode;
+  cancelLabel?: string;
+  onClose: () => void;
+};
+
+const iconFromIntent = (intent: DialogIntent | undefined) => {
+  switch (intent) {
+    case "success":
+      return <Icon name="check-circle" color="success" aria-label="Success" />;
+    case "warning":
+      return <Icon name="exclamation-triangle" color="warning" aria-label="Warning" />;
+    case "danger":
+      return <Icon name="exclamation-triangle" color="danger" aria-label="Danger" />;
+    case "info":
+      return <Icon name="info-circle" color="info" aria-label="Info" />;
+  }
+  return null;
+};
+
+export const StakingDialog: React.FC<DialogProps> = ({
+  intent,
+  title,
+  icon,
+  cancelLabel,
+  onClose,
+  children
+}) => (
+  <Card sx={{ p: 0, borderRadius: "4px" }}>
+    {intent ? <Box sx={{ height: "4px", bg: intent, borderRadius: "3px 3px 0 0" }} /> : null}
+    <Flex
+      sx={{
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center",
+        borderBottom: 1,
+        borderColor: "muted",
+        p: [3, 4],
+        pb: 3
+      }}
+    >
+      {icon || iconFromIntent(intent)}
+      <Heading as="h1" sx={{ textAlign: "center", color: "black", fontSize: [2, 3], px: [3, 0] }}>
+        {title}
+      </Heading>
+      <Button variant="icon" onClick={onClose}>
+        <Icon name="times" size="lg" aria-label={cancelLabel || "Cancel"} />
+      </Button>
+    </Flex>
+    <Stability />
+  </Card>
+);
--- packages/dev-frontend/src/pages/Dashboard.tsx
+++ packages/dev-frontend/src/pages/Dashboard.tsx
@@ -1,46 +1,11 @@
-import { Container, Grid, Card, Image, Text } from "theme-ui";
-import { Decimal, Percent, LiquityStoreState } from "@liquity/lib-base";
-import { useLiquitySelector } from "@liquity/lib-react";
-import { useLiquity } from "../hooks/LiquityContext";
-import { Trove } from "../components/Trove/Trove";
-import { Stability } from "../components/Stability/Stability";
-import { SystemStats } from "../components/SystemStats";
-import { PriceManager } from "../components/PriceManager";
-import { Staking } from "../components/Staking/Staking";
+import { Grid } from "theme-ui";
+import { Percent } from "@liquity/lib-base";
 import { CollateralCard } from "../components/ColleteralCard/ColleteralCard";
-
-const select = ({
-  numberOfTroves,
-  price,
-  total,
-  lusdInStabilityPool,
-  borrowingRate,
-  redemptionRate,
-  totalStakedLQTY,
-  frontend
-}: LiquityStoreState) => ({
-  numberOfTroves,
-  price,
-  total,
-  lusdInStabilityPool,
-  borrowingRate,
-  redemptionRate,
-  totalStakedLQTY,
-  kickbackRate: frontend.status === "registered" ? frontend.kickbackRate : null
-});
+import { useDashboard } from "../hooks/DashboardContext";
 
 export const Dashboard: React.FC = () => {
-  const {
-    numberOfTroves,
-    price,
-    lusdInStabilityPool,
-    total,
-    borrowingRate,
-    totalStakedLQTY,
-    kickbackRate
-  } = useLiquitySelector(select);
+  const { vaults } = useDashboard();
 
-  const totalCollateralRatioPct = new Percent(total.collateralRatio(price));
   return (
     <Grid
       sx={{
@@ -51,16 +16,16 @@ export const Dashboard: React.FC = () => {
         height: "100%"
       }}
     >
-      <CollateralCard
-        collateralType={"eth"}
-        totalCollateralRatioPct={totalCollateralRatioPct.prettify()}
-        total={total}
-      />
-      <CollateralCard
-        collateralType={"mco"}
-        totalCollateralRatioPct={totalCollateralRatioPct.prettify()}
-        total={total}
-      />
+      {vaults.map(vault => {
+        const totalCollateralRatioPct = new Percent(vault.collateralRatio);
+        return (
+          <CollateralCard
+            collateralType={vault.type}
+            totalCollateralRatioPct={totalCollateralRatioPct.prettify()}
+            total={vault.trove}
+          />
+        );
+      })}
     </Grid>
   );
 };
