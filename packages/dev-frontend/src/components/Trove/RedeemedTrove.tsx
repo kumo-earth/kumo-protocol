a31653c6b7b2e325466a7de76442c5b08949ed74
asimpk <asimpk.dev@gmail.com>
2022-04-25 19:34:52 +0500

    rebasing, conflicts resolved

--- packages/dev-frontend/src/components/Trove/RedeemedTrove.tsx
+++ packages/dev-frontend/src/components/Trove/RedeemedTrove.tsx
@@ -19,8 +19,28 @@ export const RedeemedTrove: React.FC = () => {
   }, [dispatchEvent]);
 
   return (
-    <Card>
-      <Heading>Trove</Heading>
+    <Card
+      sx={{
+        background: "rgba(249,248,249,.1)",
+        backgroundColor: "#303553",
+        // color: "rgba(0, 0, 0, 0.87)",
+        transition: "box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
+        boxShadow:
+          "0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%)",
+        overflow: "hidden",
+        borderRadius: "20px",
+        width: "90%",
+        color: "white"
+      }}
+    >
+      <Heading
+        sx={{
+          background: "linear-gradient(103.69deg, #2b2b2b 18.43%, #525252 100%)",
+          color: "white"
+        }}
+      >
+        Trove
+      </Heading>
       <Box sx={{ p: [2, 3] }}>
         <InfoMessage title="Your Trove has been redeemed.">
           {hasSurplusCollateral
@@ -30,7 +50,20 @@ export const RedeemedTrove: React.FC = () => {
 
         <Flex variant="layout.actions">
           {hasSurplusCollateral && <CollateralSurplusAction />}
-          {!hasSurplusCollateral && <Button onClick={handleOpenTrove}>Open Trove</Button>}
+          {!hasSurplusCollateral && (
+            <Button
+              sx={{
+                backgroundColor: "rgb(152, 80, 90)",
+                boxShadow:
+                  "rgb(0 0 0 / 20%) 0px 2px 4px -1px, rgb(0 0 0 / 14%) 0px 4px 5px 0px, rgb(0 0 0 / 12%) 0px 1px 10px 0px",
+                border: "none",
+                color: "white"
+              }}
+              onClick={handleOpenTrove}
+            >
+              Open Trove
+            </Button>
+          )}
         </Flex>
       </Box>
     </Card>
