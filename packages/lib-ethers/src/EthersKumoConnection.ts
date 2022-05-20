fe6bad9d5a97db05427897027a3ef871bb56e285
asimpk <asimpk.dev@gmail.com>
2022-05-20 17:53:12 +0500

    netwroks settings

--- packages/lib-ethers/src/EthersKumoConnection.ts
+++ packages/lib-ethers/src/EthersKumoConnection.ts
@@ -4,10 +4,6 @@ import { Signer } from "@ethersproject/abstract-signer";
 import { Decimal } from "@kumodao/lib-base";
 
 import devOrNull from "../deployments/dev.json";
-import goerli from "../deployments/goerli.json";
-import kovan from "../deployments/kovan.json";
-import rinkeby from "../deployments/rinkeby.json";
-import ropsten from "../deployments/ropsten.json";
 import mainnet from "../deployments/mainnet.json";
 import mumbai from "../deployments/mumbai.json";
 
@@ -29,10 +25,6 @@ const deployments: {
   [chainId: number]: _KumoDeploymentJSON | undefined;
 } = {
   [mainnet.chainId]: mainnet,
-  [ropsten.chainId]: ropsten,
-  [rinkeby.chainId]: rinkeby,
-  [goerli.chainId]: goerli,
-  [kovan.chainId]: kovan,
   [mumbai.chainId]: mumbai,
 
   ...(dev !== null ? { [dev.chainId]: dev } : {})
