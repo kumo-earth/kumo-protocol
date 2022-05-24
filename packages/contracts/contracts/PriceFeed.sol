369113526c7f19df4c0462b7058c7e65da24cc2e
Kevin Wollowski <k-wollowsi@live.de>
2022-05-24 13:59:55 -0700

    PriceFeed Upgradeable

--- packages/contracts/contracts/PriceFeed.sol
+++ packages/contracts/contracts/PriceFeed.sol
@@ -2,8 +2,6 @@
 
 pragma solidity 0.8.11;
 
-// import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-
 import "./Interfaces/IPriceFeed.sol";
 import "./Interfaces/ITellorCaller.sol";
 import "./Dependencies/AggregatorV3Interface.sol";
@@ -14,6 +12,10 @@ import "./Dependencies/BaseMath.sol";
 import "./Dependencies/KumoMath.sol";
 import "./Dependencies/console.sol";
 
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
 /*
 * PriceFeed for mainnet deployment, to be connected to Chainlink's live ETH:USD aggregator reference 
 * contract, and a wrapper contract TellorCaller, which connects to TellorMaster contract.
@@ -22,10 +24,10 @@ import "./Dependencies/console.sol";
 * switching oracles based on oracle failures, timeouts, and conditions for returning to the primary
 * Chainlink oracle.
 */
-contract PriceFeed is Ownable, CheckContract, BaseMath, IPriceFeed {
+contract PriceFeed is Initializable, OwnableUpgradeable, CheckContract, BaseMath, UUPSUpgradeable, IPriceFeed {
     using SafeMath for uint256;
 
-	// bool public isInitialized;
+	bool public isInitialized;
 
     string constant public NAME = "PriceFeed";
 
@@ -93,13 +95,13 @@ contract PriceFeed is Ownable, CheckContract, BaseMath, IPriceFeed {
         address _tellorCallerAddress
     )
         external
-        onlyOwner {
-		// require(!isInitialized);
+        initializer {
+		require(!isInitialized);
         checkContract(_priceAggregatorAddress);
         checkContract(_tellorCallerAddress);
-		// isInitialized = true;
+		isInitialized = true;
 
-		// __Ownable_init();
+		__Ownable_init();
        
         priceAggregator = AggregatorV3Interface(_priceAggregatorAddress);
         tellorCaller = ITellorCaller(_tellorCallerAddress);
@@ -116,8 +118,10 @@ contract PriceFeed is Ownable, CheckContract, BaseMath, IPriceFeed {
 
         _storeChainlinkPrice(chainlinkResponse);
 
-        _renounceOwnership();
+        renounceOwnership();
     }
+    
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 
     // --- Functions ---
 
