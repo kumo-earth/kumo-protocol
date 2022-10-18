b29e18f5b8ee236d6d095d383e309755daccb264
udosson <r.klemens@yahoo.de>
2022-10-18 17:29:43 +0200

    implemented asset in state Decline Test

--- packages/contracts/test/stakeDeclineTest.js
+++ packages/contracts/test/stakeDeclineTest.js
@@ -9,6 +9,7 @@ const toBN = th.toBN
 const assertRevert = th.assertRevert
 const mv = testHelpers.MoneyValues
 const timeValues = testHelpers.TimeValues
+const TroveData = testHelpers.TroveData
 
 
 /* NOTE: Some tests involving ETH redemption fees do not test for specific fee values.
@@ -20,6 +21,7 @@ const timeValues = testHelpers.TimeValues
  */
 contract('TroveManager', async accounts => {
 
+  const ZERO_ADDRESS = th.ZERO_ADDRESS
   const [owner, A, B, C, D, E, F] = accounts.slice(0, 7);
 
   const [bountyAddress, lpRewardsAddress, multisig] = accounts.slice(997, 1000)
@@ -34,15 +36,18 @@ contract('TroveManager', async accounts => {
   let defaultPool
   let borrowerOperations
   let hintHelpers
+  let KUMOContracts
+  let hardhatTester
+  let erc20Asset1
 
   let contracts
 
-  const getOpenTroveKUSDAmount = async (totalDebt) => th.getOpenTroveKUSDAmount(contracts, totalDebt)
- 
-  const getSnapshotsRatio = async () => {
-    const ratio = (await troveManager.totalStakesSnapshot())
+  const getOpenTroveKUSDAmount = async (asset, totalDebt) => th.getOpenTroveKUSDAmount(contracts, totalDebt, asset)
+
+  const getSnapshotsRatio = async (asset) => {
+    const ratio = (await troveManager.totalStakesSnapshot(asset))
       .mul(toBN(dec(1, 18)))
-      .div((await troveManager.totalCollateralSnapshot()))
+      .div((await troveManager.totalCollateralSnapshot(asset)))
 
     return ratio
   }
@@ -55,7 +60,8 @@ contract('TroveManager', async accounts => {
       contracts.stabilityPool.address,
       contracts.borrowerOperations.address
     )
-    const KUMOContracts = await deploymentHelper.deployKUMOContracts(bountyAddress, lpRewardsAddress, multisig)
+    KUMOContracts = await deploymentHelper.deployKUMOContracts(bountyAddress, lpRewardsAddress, multisig)
+    hardhatTester = await deploymentHelper.deployTesterContractsHardhat()
 
     priceFeed = contracts.priceFeedTestnet
     kusdToken = contracts.kusdToken
@@ -72,56 +78,73 @@ contract('TroveManager', async accounts => {
     kumoToken = KUMOContracts.kumoToken
     communityIssuance = KUMOContracts.communityIssuance
     lockupContractFactory = KUMOContracts.lockupContractFactory
+    erc20Asset1 = hardhatTester.erc20
+    assetAddress1 = erc20Asset1.address
+
 
     await deploymentHelper.connectCoreContracts(contracts, KUMOContracts)
     await deploymentHelper.connectKUMOContracts(KUMOContracts)
     await deploymentHelper.connectKUMOContractsToCore(KUMOContracts, contracts)
+
+    // Add asset to the system
+    await deploymentHelper.addNewAssetToSystem(contracts, KUMOContracts, assetAddress1)
+
+    // Mint token to each acccount
+    let index = 0;
+    for (const acc of accounts) {
+      // await vstaToken.approve(vstaStaking.address, await erc20Asset1.balanceOf(acc), { from: acc })
+      await erc20Asset1.mint(acc, await web3.eth.getBalance(acc))
+      index++;
+
+      if (index >= 20)
+        break;
+    }
   })
 
   it("A given trove's stake decline is negligible with adjustments and tiny liquidations", async () => {
     await priceFeed.setPrice(dec(100, 18))
-  
+
     // Make 1 mega troves A at ~50% total collateral
-    await borrowerOperations.openTrove(th._100pct, await getOpenTroveKUSDAmount(dec(1, 31)), erc20.address, erc20.address, { from: A, value: dec(2, 29) })
-    
+    await borrowerOperations.openTrove(assetAddress1, dec(2, 29), th._100pct, await getOpenTroveKUSDAmount(assetAddress1, dec(1, 31)), ZERO_ADDRESS, ZERO_ADDRESS, { from: A })
+
     // Make 5 large troves B, C, D, E, F at ~10% total collateral
-    await borrowerOperations.openTrove(th._100pct, await getOpenTroveKUSDAmount(dec(2, 30)), erc20.address, erc20.address, { from: B, value: dec(4, 28) })
-    await borrowerOperations.openTrove(th._100pct, await getOpenTroveKUSDAmount(dec(2, 30)), erc20.address, erc20.address, { from: C, value: dec(4, 28) })
-    await borrowerOperations.openTrove(th._100pct, await getOpenTroveKUSDAmount(dec(2, 30)), erc20.address, erc20.address, { from: D, value: dec(4, 28) })
-    await borrowerOperations.openTrove(th._100pct, await getOpenTroveKUSDAmount(dec(2, 30)), erc20.address, erc20.address, { from: E, value: dec(4, 28) })
-    await borrowerOperations.openTrove(th._100pct, await getOpenTroveKUSDAmount(dec(2, 30)), erc20.address, erc20.address, { from: F, value: dec(4, 28) })
-  
+    await borrowerOperations.openTrove(assetAddress1, dec(4, 28), th._100pct, await getOpenTroveKUSDAmount(assetAddress1, dec(2, 30)), ZERO_ADDRESS, ZERO_ADDRESS, { from: B })
+    await borrowerOperations.openTrove(assetAddress1, dec(4, 28), th._100pct, await getOpenTroveKUSDAmount(assetAddress1, dec(2, 30)), ZERO_ADDRESS, ZERO_ADDRESS, { from: C })
+    await borrowerOperations.openTrove(assetAddress1, dec(4, 28), th._100pct, await getOpenTroveKUSDAmount(assetAddress1, dec(2, 30)), ZERO_ADDRESS, ZERO_ADDRESS, { from: D })
+    await borrowerOperations.openTrove(assetAddress1, dec(4, 28), th._100pct, await getOpenTroveKUSDAmount(assetAddress1, dec(2, 30)), ZERO_ADDRESS, ZERO_ADDRESS, { from: E })
+    await borrowerOperations.openTrove(assetAddress1, dec(4, 28), th._100pct, await getOpenTroveKUSDAmount(assetAddress1, dec(2, 30)), ZERO_ADDRESS, ZERO_ADDRESS, { from: F })
+
     // Make 10 tiny troves at relatively negligible collateral (~1e-9 of total)
     const tinyTroves = accounts.slice(10, 20)
     for (account of tinyTroves) {
-      await borrowerOperations.openTrove(th._100pct, await getOpenTroveKUSDAmount(dec(1, 22)), erc20.address, erc20.address, { from: account, value: dec(2, 20) })
+      await borrowerOperations.openTrove(assetAddress1, dec(2, 20), th._100pct, await getOpenTroveKUSDAmount(assetAddress1, dec(1, 22)), ZERO_ADDRESS, ZERO_ADDRESS, { from: account })
     }
 
     // liquidate 1 trove at ~50% total system collateral
     await priceFeed.setPrice(dec(50, 18))
-    assert.isTrue(await troveManager.checkRecoveryMode(await priceFeed.getPrice()))
-    await troveManager.liquidate(A)
+    assert.isTrue(await troveManager.checkRecoveryMode(assetAddress1, await priceFeed.getPrice()))
+    await troveManager.liquidate(assetAddress1, A)
 
-    console.log(`totalStakesSnapshot after L1: ${await troveManager.totalStakesSnapshot()}`)
-    console.log(`totalCollateralSnapshot after L1: ${await troveManager.totalCollateralSnapshot()}`)
-    console.log(`Snapshots ratio after L1: ${await getSnapshotsRatio()}`)
-    console.log(`B pending ETH reward after L1: ${await troveManager.getPendingETHReward(B)}`)
-    console.log(`B stake after L1: ${(await troveManager.Troves(B))[2]}`)
+    console.log(`totalStakesSnapshot after L1: ${await troveManager.totalStakesSnapshot(assetAddress1)}`)
+    console.log(`totalCollateralSnapshot after L1: ${await troveManager.totalCollateralSnapshot(assetAddress1)}`)
+    console.log(`Snapshots ratio after L1: ${await getSnapshotsRatio(assetAddress1)}`)
+    console.log(`B pending ETH reward after L1: ${await troveManager.getPendingReward(assetAddress1, B)}`)
+    console.log(`B stake after L1: ${(await troveManager.Troves(B, assetAddress1))[TroveData.stake]}`)
 
     // adjust trove B 1 wei: apply rewards
-    await borrowerOperations.adjustTrove(th._100pct, 0, 1, false, erc20.address, erc20.address, {from: B})  // B repays 1 wei
-    console.log(`B stake after A1: ${(await troveManager.Troves(B))[2]}`)
-    console.log(`Snapshots ratio after A1: ${await getSnapshotsRatio()}`)
+    await borrowerOperations.adjustTrove(assetAddress1, 0, th._100pct, 0, 1, false, ZERO_ADDRESS, ZERO_ADDRESS, { from: B })  // B repays 1 wei
+    console.log(`B stake after A1: ${(await troveManager.Troves(B, assetAddress1))[TroveData.stake]}`)
+    console.log(`Snapshots ratio after A1: ${await getSnapshotsRatio(assetAddress1)}`)
 
     // Loop over tiny troves, and alternately:
     // - Liquidate a tiny trove
     // - Adjust B's collateral by 1 wei
     for (let [idx, trove] of tinyTroves.entries()) {
-      await troveManager.liquidate(trove)
-      console.log(`B stake after L${idx + 2}: ${(await troveManager.Troves(B))[2]}`)
-      console.log(`Snapshots ratio after L${idx + 2}: ${await getSnapshotsRatio()}`)
-      await borrowerOperations.adjustTrove(th._100pct, 0, 1, false, erc20.address, erc20.address, {from: B})  // A repays 1 wei
-      console.log(`B stake after A${idx + 2}: ${(await troveManager.Troves(B))[2]}`)
+      await troveManager.liquidate(assetAddress1, trove)
+      console.log(`B stake after L${idx + 2}: ${(await troveManager.Troves(B, assetAddress1))[TroveData.stake]}`)
+      console.log(`Snapshots ratio after L${idx + 2}: ${await getSnapshotsRatio(assetAddress1)}`)
+      await borrowerOperations.adjustTrove(assetAddress1, 0, th._100pct, 0, 1, false, ZERO_ADDRESS, ZERO_ADDRESS, { from: B })  // A repays 1 wei
+      console.log(`B stake after A${idx + 2}: ${(await troveManager.Troves(B, assetAddress1))[TroveData.stake]}`)
     }
   })
 
