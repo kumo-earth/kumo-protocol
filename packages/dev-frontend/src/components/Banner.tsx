86210564ead7fb6f48fc3fbb37e11cf67579b9bc
asimpk <asimpk.dev@gmail.com>
2023-03-31 00:15:17 +0500

    close btn added for info banner

--- packages/dev-frontend/src/components/Banner.tsx
+++ packages/dev-frontend/src/components/Banner.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Progress, Alert, Heading } from "theme-ui";
 import { useViewBanner } from "../hooks/useViewBanner";
+import { Icon } from "./Icon";
 
 export const Banner: React.FC<{ bannerHeading : string,  visibility: number }> = ({ bannerHeading, visibility, children }) => {
   const { isViewBannerCheck, changeInProgress } = useViewBanner(visibility);
-  return !isViewBannerCheck ? (
+  const [isView, setIsView] = useState(true);
+  
+
+  return (!isViewBannerCheck && isView) ? (
     <Alert
       sx={{
         position: "absolute",
@@ -23,8 +27,11 @@ export const Banner: React.FC<{ bannerHeading : string,  visibility: number }> =
         zIndex: 9999999
       }}
     >
-      <Heading as="h3" sx={{ mb: 2, color: "black" }}>
+      <Heading as="h3" sx={{color: "black", width: "100%", display: "flex", justifyContent: "space-between", mb: 2,mr: 2 }}>
         {bannerHeading}
+        <span style={{ marginLeft: "auto", cursor: "pointer" }} onClick={() => setIsView(false)}>
+          <Icon name="window-close" size={"1x"} color="white" />
+        </span>
       </Heading>
       <Heading as="h4" sx={{ mb: 2, color: "black" }}>
         {children}
