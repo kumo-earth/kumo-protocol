c5a5cc321948169c71df9537cf4e49dda5cddc17
asimpk <asimpk.dev@gmail.com>
2023-06-18 12:31:31 +0500

    CI Impr Attempt

--- packages/dev-frontend/tests/e2e/specs/connect-wallet-spec.js
+++ packages/dev-frontend/tests/e2e/specs/connect-wallet-spec.js
@@ -1,11 +1,8 @@
 describe('connect wallet spec', () => {
     it('should connect wallet with success', () => {
       cy.visit('/');
-      cy.wait(30000);
-      cy.contains('CONNECT').click();
-      cy.wait(5000);
-      cy.contains('MetaMask').click();
-      cy.wait(5000);
+      cy.contains('CONNECT', {timeout: 5000 }).click();
+      cy.contains('MetaMask', {timeout: 5000 }).click();
       cy.switchToMetamaskWindow();
       cy.acceptMetamaskAccess().should("be.true");
       cy.switchToCypressWindow();
