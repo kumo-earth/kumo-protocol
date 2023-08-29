f1f682a7eae0ba35b2c77796b608da73a9d47115
Arthur Galiullin <galiullinko@gmail.com>
2023-08-29 14:02:12 +0200

    feat(sol): comment out KUMOStaking
    To make the pipeline run correctly, I need to comment out KUMOStaking and related contracts

--- packages/contracts/contracts/TestContracts/KUMOStakingTester.sol
+++ packages/contracts/contracts/TestContracts/KUMOStakingTester.sol
@@ -4,9 +4,8 @@ pragma solidity 0.8.11;
 
 import "../KUMO/KUMOStaking.sol";
 
-
 contract KUMOStakingTester is KUMOStaking {
-    function requireCallerIsTroveManager() external view {
-        _requireCallerIsTroveManager();
-    }
+    // function requireCallerIsTroveManager() external view {
+    //     _requireCallerIsTroveManager();
+    // }
 }
